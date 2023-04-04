9d62c1cd502cc49692b58343f9250df69a333056
tcanh20102000 <tcanh18@apcs.vn>
2023-04-04 23:02:17 +0700

    Revert "Delete src/images directory"
    This reverts commit b6b7425e1218a78d2d49222f7dd80140e0c35b5d.

---
+++ src/images/images.js
@@ -0,0 +1,10 @@
+import AirBnb from './airbnb_logo.png'
+import PhotoGib from './photo-grip.png'
+import VerticalPortrait from './vertical-portrait.jpg'
+
+const images = {
+    AirBnb,
+    PhotoGib,
+    VerticalPortrait
+}
+export default images
